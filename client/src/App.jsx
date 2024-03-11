4407f2e3ee82e7993aedd1250417bbe19fcfc2e4
richmond-andoh <richmondandoh00@gmail.com>
2024-03-11 13:00:24 +0000

    Login page designed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
-
+import Login from "./pages/Login"
 const App = () => {
   return (
     <div className='flex p-4 h-screen items-center justify-center'>
-
+      <Login />
     </div>
   )
 }
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,8 +2,31 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>
-      
+    <div className='items-start justify-center flex flex-col min-w-48 mx-auto'>
+      <div className='w-full p-6 rounded-lg shadow-2xl bg-zinc-900 bg-clip-padding backdrop-blur-sm backdrop-filter bg-opacity-0'>
+            <h1 className='text-3xl text-center font-semibold text-gray-300 mb-5'>
+                Login
+                <span className='text-blue-500 pl-3'>
+                    ChatLoom
+                </span>
+            </h1>
+
+            <form action="">
+                <div className='p-7'>
+                    <label className="input input-bordered flex items-center gap-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg>
+                        <input type="text" className="grow" placeholder="Username" />
+                    </label>
+                    <label className="input input-bordered w-full flex items-center gap-2 mt-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" /><path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" /></svg>
+                        <input type="text" className="grow" placeholder="Email" />
+                    </label>
+                </div>
+                <div className='px-7'>
+                  <button className="btn btn-md btn-active btn-primary w-full">Primary</button>
+                </div>
+            </form>
+      </div>
     </div>
   )
 }
