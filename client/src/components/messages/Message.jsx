4727a0dd9895506dde97ede54ab2c6f99b0d13bb
richmond-andoh <richmondandoh00@gmail.com>
2024-03-22 13:02:34 +0000

    functionality to extract time format in mongodb

--- client/src/components/messages/Message.jsx
+++ client/src/components/messages/Message.jsx
@@ -1,23 +1,30 @@
+import { useAuthContext } from "../../context/Auth_Context";
+import useChat from "../../zustand_store/useChat";
 
-const Message = () => {
+const Message = ({ message }) => {
+
+  const { authUser } = useAuthContext();
+  const { selectedChat } = useChat();
+
+  const myMessage = message.senderId === authUser._id;
+  const chatClassName = myMessage ? "chat-end" : "chat-start";
+  const myProfileImg = myMessage ? authUser.myProfileImg : selectedChat?.ProfileImg;
+  const chatBgColor = myMessage ? "chat-bubble-secondary" : "";
   return (
     <>
-      <div className="chat chat-start">
+      <div className={`chat ${chatClassName}`}>
         <div className="chat-image avatar">
           <div className="w-10 rounded-full">
-            <img alt="Tailwind CSS chat bubble component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+            <img alt="Tailwind CSS chat bubble component" src={myProfileImg} />
           </div>
         </div>
-        <div className="chat-header">
-          Obi-Wan Kenobi
-          <time className="text-xs opacity-50">12:45</time>
-        </div>
-        <div className="chat-bubble">You were the Chosen One!</div>
+       
+        <div className={`chat-bubble text-white ${chatBgColor}`}>{message.message}</div>
         <div className="chat-footer opacity-50">
-          Delivered
+          
         </div>
       </div>
-      <div className="chat chat-end">
+      {/* <div className="chat chat-end">
           <div className="chat-image avatar">
             <div className="w-10 rounded-full">
               <img alt="Tailwind CSS chat bubble component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
@@ -32,7 +39,7 @@ const Message = () => {
           <div className="chat-footer opacity-50">
             Seen at 12:46
           </div>
-      </div>
+      </div> */}
     </>
   )
 }
