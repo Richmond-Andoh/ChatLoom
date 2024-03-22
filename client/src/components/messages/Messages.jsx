c2224cc38dafa8b0b1e7ae99dbb9f53e7439546b
richmond-andoh <richmondandoh00@gmail.com>
2024-03-22 12:35:06 +0000

    some fixes in the Messages component

--- client/src/components/messages/Messages.jsx
+++ client/src/components/messages/Messages.jsx
@@ -1,12 +1,18 @@
 import useGetMessages from "../../hooks/useGetMessages";
 import MessageLoadingSkeleton from "../loading_skeleton/Message_Loading_Skeleton";
+import Message from "./Message";
 
 const Messages = () => {
   const { loading, messages } = useGetMessages;
+  
   return (
     <div className='px-4 flex-1 overflow-auto'>
-
+    {!loading && messages.length > 0 (
+      messages.map((message) => <Message key={message._id} message={message}/>)
+    )}
     {  loading && [...Array(3)].map((_, idx) => <MessageLoadingSkeleton key={idx}/>)}
+
+    {!loading && messages.length === 0 && <p className="text-center">Send a text to begin conversation.</p>}
     </div>
   )
 }
